7ae57c4a13205698133bea32d5b00b124ffae390
Dennis Schulte <mail@dennis-schulte.de>
2015-04-24 14:39:27 +0200

    Overwrite changes, because it broke the build.

--- src/main/java/de/codecentric/mule/configuration/MuleContextListener.java
+++ src/main/java/de/codecentric/mule/configuration/MuleContextListener.java
@@ -4,7 +4,6 @@ import org.mule.api.MuleContext;
 import org.mule.api.MuleException;
 import org.mule.config.spring.SpringXmlConfigurationBuilder;
 import org.mule.context.DefaultMuleContextFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ApplicationContextEvent;
 import org.springframework.context.event.ContextClosedEvent;
@@ -14,7 +13,6 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * ApplicationListener to startup Mule
  * 
  * @author Dennis Schulte
- * @author Johannes Stelzer
  */
 public class MuleContextListener implements ApplicationListener<ApplicationContextEvent> {
 
@@ -22,40 +20,26 @@ public class MuleContextListener implements ApplicationListener<ApplicationConte
 
 	@Override
 	public void onApplicationEvent(ApplicationContextEvent event) {
-		if (event instanceof ContextRefreshedEvent) {
-			startMuleContext(event.getApplicationContext());
-		} else if (event instanceof ContextClosedEvent) {
-			stopMuleContext();
-		}
-	}
-
-	private void startMuleContext(ApplicationContext parent) {
-		if (muleContext == null) {
+		if(event instanceof ContextRefreshedEvent){
 			DefaultMuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
 			SpringXmlConfigurationBuilder configBuilder;
 			try {
 				configBuilder = new SpringXmlConfigurationBuilder("mule-config.xml");
-				configBuilder.setParentContext(parent);
 				muleContext = muleContextFactory.createMuleContext(configBuilder);
 				muleContext.start();
 			} catch (MuleException e) {
 				throw new RuntimeException(e);
+			}			
+		}
+		if(event instanceof ContextClosedEvent && muleContext!=null){
+			try {
+				muleContext.stop();
+				muleContext=null;
+			} catch (MuleException e) {
+				throw new RuntimeException(e);
 			}
 		}
+
 	}
 
-	private void stopMuleContext() {
-		try {
-		if (muleContext != null) {
-			muleContext.stop();
-			muleContext = null;
-		}
-		} catch (MuleException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public MuleContext getMuleContext() {
-		return muleContext;
-	}
 }
