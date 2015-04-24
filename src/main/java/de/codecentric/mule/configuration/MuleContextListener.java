ae66455acf51919238c5b3134cb5acde5f088d1d
Johannes Stelzer <Johannes.Stelzer@googlemail.com>
2015-04-24 13:23:51 +0200

    * parent context for Mule * stop MuleContext on ContextClose * Don't start multiple MuleContexts

--- src/main/java/de/codecentric/mule/configuration/MuleContextListener.java
+++ src/main/java/de/codecentric/mule/configuration/MuleContextListener.java
@@ -4,27 +4,58 @@ import org.mule.api.MuleContext;
 import org.mule.api.MuleException;
 import org.mule.config.spring.SpringXmlConfigurationBuilder;
 import org.mule.context.DefaultMuleContextFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ApplicationContextEvent;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * ApplicationListener to startup Mule
  * 
  * @author Dennis Schulte
+ * @author Johannes Stelzer
  */
-public class MuleContextListener implements ApplicationListener<ContextRefreshedEvent> {
+public class MuleContextListener implements ApplicationListener<ApplicationContextEvent> {
+
+	private MuleContext muleContext;
 
 	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		DefaultMuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
-		SpringXmlConfigurationBuilder configBuilder;
+	public void onApplicationEvent(ApplicationContextEvent event) {
+		if (event instanceof ContextRefreshedEvent) {
+			startMuleContext(event.getApplicationContext());
+		} else if (event instanceof ContextClosedEvent) {
+			stopMuleContext();
+		}
+	}
+
+	private void startMuleContext(ApplicationContext parent) {
+		if (muleContext == null) {
+			DefaultMuleContextFactory muleContextFactory = new DefaultMuleContextFactory();
+			SpringXmlConfigurationBuilder configBuilder;
+			try {
+				configBuilder = new SpringXmlConfigurationBuilder("mule-config.xml");
+				configBuilder.setParentContext(parent);
+				muleContext = muleContextFactory.createMuleContext(configBuilder);
+				muleContext.start();
+			} catch (MuleException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private void stopMuleContext() {
 		try {
-			configBuilder = new SpringXmlConfigurationBuilder("mule-config.xml");
-			MuleContext muleContext = muleContextFactory.createMuleContext(configBuilder);
-			muleContext.start();
+		if (muleContext != null) {
+			muleContext.stop();
+			muleContext = null;
+		}
 		} catch (MuleException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
+	public MuleContext getMuleContext() {
+		return muleContext;
+	}
 }
